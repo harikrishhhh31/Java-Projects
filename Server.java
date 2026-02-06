500d457b029316b7200a81efc2cb7898d328fa2e
harikrishhhh31 <nerdyninja59@gmail.com>
2026-02-07 00:00:59 +0530

    Learning about endpoint inspector and http lifecycle

--- Server.java
+++ Server.java
@@ -3,6 +3,7 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import java.net.InetSocketAddress;
 import java.io.IOException;
+import com.sun.net.httpserver.Headers;
 
 class Server{
     public static void main(String[]args)throws IOException{
@@ -18,6 +19,23 @@ class Server{
                 exchange.getResponseBody().close();
             }
         });
+        server.createContext("/info", new HttpHandler(){
+        @Override
+        public void handle(HttpExchange exchange)throws IOException{
+           String method=exchange.getRequestMethod();
+           String path =exchange.getRequestURI().getPath();
+           Headers header=exchange.getRequestHeaders();
+           String response1="Method:"+ method+"\n"+
+           "Path :"+path +"\n"+
+            "Headers:\n";
+           for(String key:header.keySet()){
+            response1+=key+ ":" +header.get(key)+"\n";
+           }
+           exchange.sendResponseHeaders(200, response1.length());
+           exchange.getResponseBody().write(response1.getBytes());
+           exchange.getResponseBody().close();
+                                        
+        }});
         server.start();
 }
 }
